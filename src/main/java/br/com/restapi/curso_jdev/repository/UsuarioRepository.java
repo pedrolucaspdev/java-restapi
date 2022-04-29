96cf4311a372192e58309b43ce1e09d25053437f
pedrolucasjdev <pedrolucassec@gmail.com>
2022-04-29 17:50:37 -0300

    Tirando dependências desnecessárias..

--- src/main/java/br/com/restapi/curso_jdev/repository/UsuarioRepository.java
+++ src/main/java/br/com/restapi/curso_jdev/repository/UsuarioRepository.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import br.com.restapi.curso_jdev.model.Usuario;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
 	@Query(value = "select u from Usuario u where upper(trim(u.nome)) like %?1% order by u.id")
